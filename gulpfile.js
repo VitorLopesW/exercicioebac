68b484f5a628a0e5feb87b2e087a3910667f809a
VitorLopesW <67442259+VitorLopesW@users.noreply.github.com>
2024-01-18 14:27:35 -0300

    u

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,42 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const sourcemaps = require('gulp-sourcemaps')
+var uglify = require('gulp-uglify')
+const rename = require('gulp-rename')
+const obfuscate = require('gulp-obfuscate')
+const imagemin = require('gulp-imagemin')
+
+function comprimeImagens(){
+    return gulp.src('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./built/images'))
+}
 
 function compileSass(){
     console.log("Executando sass, via Gulp")
-    return gulp.src('./source/*.sass')
+    return gulp.src('./source/styles*.sass')
     .pipe(sourcemaps.init())
     .pipe(sass({
         outputStyle: 'compressed'
     }))
     .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('./built'))
+    .pipe(gulp.dest('./built/styles'))
 }
 
-exports.default = compileSass
 
-exports.watch = function() {
-    gulp.watch('./source/*.sass',{ ignoreInitial: false }, gulp.series(compileSass))
+
+function minifyJS() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./built/scripts'));
+}
+
+exports.default = function() {
+    gulp.watch('./source/styles*.sass',{ ignoreInitial: false }, gulp.series(compileSass))
+
+    gulp.watch('./source/scripts/*.js', { ignoreInitial: false }, gulp.series(minifyJS))
+
+    gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(comprimeImagens))
 }
---
+++ source/scripts/main.js
@@ -0,0 +1,6 @@
+console.log('hello world')
+
+function learningJs(numA, numbB, symbol){
+    if(symbol === 'duck') return numA + numbB
+    if(symbol === 'chicken') return numA - numB
+}
